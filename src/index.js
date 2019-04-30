1721554df4121510e42dd62e044d8b7535c132ce
Daniel Montoya <dsmontoyam@gmail.com>
2019-04-30 12:39:46 -0500

    Remove seeming unnecessary dependency

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { app, BrowserWindow } from 'electron';
 import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer';
 import { enableLiveReload } from 'electron-compile';
-import A from "vueify-insert-css"
+
 
 
 // Keep a global reference of the window object, if you don't, the window will
