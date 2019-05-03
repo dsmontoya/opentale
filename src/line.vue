e9ef4c31da70f4c9a96c3b18514c78ffb3cc4954
Daniel Montoya <dsmontoyam@gmail.com>
2019-05-02 19:46:46 -0500

    Focus style

--- src/line.vue
+++ src/line.vue
@@ -1,5 +1,5 @@
 <template>
-  <input v-model="line.text" :tabindex="lineIndex" v-focus>
+  <input v-model="line.text" v-bind:class="{focused: isFocused, blured: !isFocused}" :tabindex="lineIndex" @focus="focus" @blur="blur" v-focus>
 </template>
 
 <script>
@@ -8,6 +8,7 @@ import Vue from 'vue';
 export default {
   data: function(){
     return {
+      isFocused: false,
       text: 'hey'
     }
   },
@@ -18,15 +19,22 @@ export default {
     line: {}
   },
   methods: {
-   update:function (e) {
+    blur: function(e) {
+      this.isFocused = false
+    },
+    focus: function(e) {
+      console.log("focused");
+      this.isFocused = true
+    },
+    update:function (e) {
      console.log("line",this.line);
-   // console.log("fountain",e)
+    // console.log("fountain",e)
      //this.input = e.target.value
-   },
-   newLine: function(e){
+    },
+    newLine: function(e){
      console.log("newLine line");
      this.data.push({})
-   }
+    }
  },
  directives: {
    focus: {
