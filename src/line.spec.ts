4baf4daff24ad5087522b65824f671c3a4891e6a
Daniel Montoya <dsmontoyam@gmail.com>
2019-05-03 17:54:21 -0500

    Configure jest

--- src/line.spec.ts
+++ src/line.spec.ts
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import MyComponent from 'path/to/MyComponent.vue'
+import Editor from './editor.vue'
 
 // Here are some Jasmine 2.0 tests, though you can
 // use any test runner / assertion library combo you prefer
