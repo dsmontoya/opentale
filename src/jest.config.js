84021be0c4f6979b196688f95fa2984df000f85b
Daniel Montoya <dsmontoyam@gmail.com>
2019-05-04 20:41:43 -0500

    Use babel-jest to transform instead of ts-jest

--- src/jest.config.js
+++ src/jest.config.js
@@ -1,18 +1,14 @@
 module.exports = {
-  moduleFileExtensions: ['js', 'jsx', 'json', 'vue', 'ts'],
-  transform: {
-    '^.+\\.vue$': 'vue-jest',
-    '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$':
-      'jest-transform-stub',
-    '^.+\\.(js|jsx)?$': 'babel-jest',
-    '^.+\\.tsx?$': 'ts-jest'
-  },
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1'
+    "^vue$": "vue/dist/vue.common.js"
   },
-  snapshotSerializers: ['jest-serializer-vue'],
-  testMatch: [
-    '<rootDir>/(*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx))'
+  moduleFileExtensions: [
+    "js",
+    "vue"
   ],
+  transform: {
+    "^.+\\.js$": "babel-jest",
+    ".*\\.(vue)$": "vue-jest"
+  },
   transformIgnorePatterns: ['<rootDir>/node_modules/']
 };
