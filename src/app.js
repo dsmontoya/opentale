cf2f304826460f9134f5723bc670f39b7101a029
Daniel Montoya <dsmontoyam@gmail.com>
2019-08-28 12:01:13 -0500

    really bad attempt to get electron running using webpack

---
+++ src/app.js
@@ -0,0 +1,3 @@
+import Vue from 'vue'
+import App from './app'
+new Vue(App)
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+var path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: 'src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      // this will apply to both plain `.js` files
+      // AND `<script>` blocks in `.vue` files
+      {
+        test: /\.js$/,
+        loader: 'babel-loader'
+      },
+      // this will apply to both plain `.css` files
+      // AND `<style>` blocks in `.vue` files
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    // make sure to include the plugin!
+    new VueLoaderPlugin()
+  ]
+};
