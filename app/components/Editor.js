d6ff5b5be56e008d89b6fe5fadc0c73e31764ebd
Daniel Montoya <dsmontoyam@gmail.com>
2019-09-12 20:11:15 -0500

    list line types

--- app/components/Editor.js
+++ app/components/Editor.js
@@ -1,8 +1,10 @@
 // @flow
 import React, { Component } from 'react';
 import ContentEditable from 'react-contenteditable'
+import Select from 'react-select'
 import { Link } from 'react-router-dom';
 import routes from '../constants/routes';
+import lineTypes from '../constants/lineTypes';
 import styles from './Editor.css';
 
 type Props = {
@@ -33,6 +35,28 @@ export default class Editor extends Component<Props> {
       this.contentEditable = React.createRef();
     };
 
+    formatLineTypes(lt: any) {
+      var newLineTypes = []
+      for (var key in lt) {
+        var t = lt[key]
+        var split = t.split("_")
+        console.log("split",split)
+        var name: string = ""
+        for (let i = 0; i < split.length; i++) {
+          const element = split[i];
+          console.log("element",element)
+          console.log("uppercase before",name)
+          name += element
+          if (i != split.length-1) {
+            name += " "
+          }
+        }
+        console.log("uppercase",name)
+        newLineTypes.push({value: key, label: name})
+      }
+      return newLineTypes
+    }
+
   render() {
     const {
      editor,
@@ -49,6 +73,7 @@ export default class Editor extends Component<Props> {
         <EditButton cmd="italic" />
         <EditButton cmd="insertHTML" arg="<div class='test'></div>" />
         line type: {lineType}
+        <Select options={this.formatLineTypes(lineTypes)} />
         <ContentEditable
         innerRef={this.contentEditable}
         html={html} // innerHTML of the editable div
