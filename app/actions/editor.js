fb2bf771c7d5d17bf952b1e014bbf543c87e7b95
Daniel Montoya <dsmontoyam@gmail.com>
2019-09-22 22:25:54 -0500

    include line class name

--- app/actions/editor.js
+++ app/actions/editor.js
@@ -1,6 +1,5 @@
 // @flow
 import type { GetState, Dispatch } from '../reducers/types';
-import lineTypes from "../constants/lineTypes";
 import styles from '../components/Editor.css';
 
 export const NEXT_LINE_TYPE = 'NEXT_LINE_TYPE';
@@ -8,48 +7,47 @@ export const PREV_LINE_TYPE = 'PREV_LINE_TYPE';
 export const SET_LINE_TYPE = 'SET_LINE_TYPE';
 export const TAB_NEXT_LINE_TYPE = 'TAB_NEXT_LINE_TYPE';
 export const TAB_PREV_LINE_TYPE = 'TAB_PREV_LINE_TYPE';
-export const UPDATE_HTML = "UPDATE_HTML"
+export const UPDATE_HTML = 'UPDATE_HTML';
 
 export const extraLineTypes = {
   FORMATTING: {
-    BOLD: "bold",
-    ITALIC: "italic",
-    UNDERLINE: "underline",
-    STRIKETHROUGH: "strikethrough",
-    CENTER: "center"
+    BOLD: 'bold',
+    ITALIC: 'italic',
+    UNDERLINE: 'underline',
+    STRIKETHROUGH: 'strikethrough',
+    CENTER: 'center'
   },
-  CUSTOM: "custom"
-}
+  CUSTOM: 'custom'
+};
 
-export function handleClick(evt: any) {
+export function handleClick(evt: React.MouseEvent<HTMLDivElement, MouseEvent>) {
   return (dispatch: Dispatch, getState: GetState) => {
     const { lineType } = getState();
-    var target = evt.target
-    var targetLineType = target.className.split("__")[1]
-    console.log(targetLineType,lineType)
-    if (targetLineType && lineType != targetLineType) {
-      dispatch(setLineType(targetLineType))
+    const { target } = evt;
+    const targetLineType = target.className.split('__')[1];
+    if (targetLineType && lineType !== targetLineType) {
+      dispatch(setLineType(targetLineType));
     }
-  }
+  };
 }
 
-export function handleSelect(option: any) {
+export function handleSelect(option: Option) {
   return (dispatch: Dispatch, getState: GetState) => {
-    const lineType = option.value.toLowerCase()
-    dispatch(setLineType(lineType))
-    updateSelection(lineType)(dispatch, getState)
-  }
+    const lineType = option.value.toLowerCase();
+    dispatch(setLineType(lineType));
+    updateSelection(lineType)(dispatch, getState);
+  };
 }
 
-export function nextLine(evt: any) {
+export function nextLine(evt: ContentEditableEvent) {
   return (dispatch: Dispatch, getState: GetState) => {
-    console.log("evt",evt)
-    var nativeEvent = evt.nativeEvent
-    dispatch(updateHTML(evt.target.value))
-    if (nativeEvent.inputType == "insertParagraph") {
+    console.log('evt', evt);
+    const { nativeEvent } = evt;
+    dispatch(updateHTML(evt.target.value));
+    if (nativeEvent.inputType === 'insertParagraph') {
       dispatch(nextLineType());
       const { lineType } = getState();
-      updateSelection(lineType)(dispatch, getState)
+      updateSelection(lineType)(dispatch, getState);
     }
   };
 }
@@ -57,63 +55,66 @@ export function nextLine(evt: any) {
 export function nextLineType() {
   return {
     type: NEXT_LINE_TYPE
-  }
+  };
 }
 
-//TODO action for new line
+// TODO action for new line
 
 export function prevLineType() {
   return {
     type: PREV_LINE_TYPE
-  }
+  };
 }
 
 export function setLineType(lineType: string) {
   return {
     type: SET_LINE_TYPE,
-    lineType: lineType
-  }
+    lineType
+  };
 }
 
 export function tabNextLineType() {
   return {
     type: TAB_NEXT_LINE_TYPE
-  }
+  };
 }
 
 export function tabPrevLineType() {
   return {
     type: TAB_PREV_LINE_TYPE
-  }
+  };
 }
 
-export function updateHTML(html: string){
-  console.log("updating html",html)
+export function updateHTML(html: string) {
   return {
     type: UPDATE_HTML,
-    html: html
-  }
+    html
+  };
 }
 
 function updateSelection(lineType: string) {
   return (dispatch: Dispatch, getState: GetState) => {
-    const selection = window.getSelection()
-    const focusNode = selection.focusNode
-    const tagName = focusNode.tagName
-    var div: Node
-    console.log("tagname",tagName)
-    if (tagName == "DIV") {
-      div = focusNode
+    const selection = window.getSelection();
+    const { focusNode } = selection;
+    const { tagName } = focusNode;
+    let div: Node;
+    console.log('tagname', tagName);
+    if (tagName === 'DIV') {
+      div = focusNode;
     } else {
-      div = focusNode.parentNode
+      div = focusNode.parentNode;
     }
-    updateDiv(div, lineType)(dispatch, getState)
-  }
+    updateDiv(div, lineType)(dispatch, getState);
+  };
 }
 
 function updateDiv(div: Node, lineType: string) {
+  // eslint-disable-next-line no-unused-vars
   return (dispatch: Dispatch, getState: GetState) => {
-    div.className = styles[lineType]
-    dispatch(updateHTML(window.document.getElementsByClassName("editor")[0].innerHTML))
-  }
+    // eslint-disable-next-line no-param-reassign
+    div.className = `${styles[lineType]} ${styles.line}`;
+    dispatch(
+      updateHTML(window.document.getElementsByClassName('editor')[0].innerHTML)
+    );
+  };
 }
