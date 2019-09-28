99688818ff8499bed48325b3ead2dd45bc84a68e
Daniel Montoya <dsmontoyam@gmail.com>
2019-09-28 14:15:15 -0500

    test handleSelect

--- test/actions/action.spec.js
+++ test/actions/action.spec.js
@@ -1,13 +1,12 @@
 import { spy } from 'sinon';
 import lineTypes from '../../app/constants/lineTypes';
 import * as actions from '../../app/actions/editor';
-import { JSDOM } from 'jsdom';
 
 describe('actions', () => {
   it('should update the line type when a line is clicked', () => {
     const evt = {
       target: {
-        className: "Editor__scene_heading__FQx_9"
+        className: 'Editor__scene_heading__FQx_9'
       }
     };
     const fn = actions.handleClick(evt);
@@ -15,27 +14,36 @@ describe('actions', () => {
     const dispatch = spy();
     const getState = () => ({ lineType: lineTypes.ACT });
     fn(dispatch, getState);
-    expect(dispatch.calledWith({ type: actions.SET_LINE_TYPE, lineType: lineTypes.SCENE_HEADING })).toBe(true);
+    expect(
+      dispatch.calledWith({
+        type: actions.SET_LINE_TYPE,
+        lineType: lineTypes.SCENE_HEADING
+      })
+    ).toBe(true);
   });
 
-  // it('should update the line type when a line type is selected from menu', () => {
-  //   window.getSelection = () => {
-  //     return {
-  //       focusNode: {
-  //         tagName: "DIV"
-  //       }
-  //     };
-  //   };
-  //   window.document = (new JSDOM(`<!DOCTYPE html><body><article>hey</article></body>`)).window.document;
-  //   console.log(window.document.getElementsByTagName("article")[0])
-  //   const option = {
-  //     value: "SCENE_HEADING"
-  //   };
-  //   const fn = actions.handleSelect(option);
-  //   expect(fn).toBeInstanceOf(Function);
-  //   const dispatch = spy();
-  //   const getState = () => ({ lineType: lineTypes.ACT });
-  //   fn(dispatch, getState);
-  //   expect(dispatch.calledWith({ type: actions.SET_LINE_TYPE, lineType: lineTypes.SCENE_HEADING })).toBe(true);
-  // });
+  it('should update the line type when a line type is selected from menu', () => {
+    window.getSelection = () => {
+      return {
+        focusNode: {
+          tagName: 'DIV'
+        }
+      };
+    };
+    window.document.body.innerHTML = `<!DOCTYPE html><body><div class='editor'>hey</div></body>`;
+    const option = {
+      value: 'SCENE_HEADING'
+    };
+    const fn = actions.handleSelect(option);
+    expect(fn).toBeInstanceOf(Function);
+    const dispatch = spy();
+    const getState = () => ({ lineType: lineTypes.ACT });
+    fn(dispatch, getState);
+    expect(
+      dispatch.calledWith({
+        type: actions.SET_LINE_TYPE,
+        lineType: lineTypes.SCENE_HEADING
+      })
+    ).toBe(true);
+  });
 });
